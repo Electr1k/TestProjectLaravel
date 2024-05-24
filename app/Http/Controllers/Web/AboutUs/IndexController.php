7ee5e7e7612b599bbf82ba3b3812c08c270fae74
Electr1k <gynter37@gmail.com>
2024-05-24 04:51:52 +0300

    add docker

--- app/Http/Controllers/Web/AboutUs/IndexController.php
+++ app/Http/Controllers/Web/AboutUs/IndexController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers\Web\AboutUs;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function __invoke(Request $request){
-        $current_city = $request->session()->get('current_city');
-        return view('about_us.index', compact('current_city'));
+    public function __invoke(){
+        return view('about_us.index');
     }
 }
--- app/Http/Controllers/Web/News/IndexController.php
+++ app/Http/Controllers/Web/News/IndexController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function __invoke(){
-        $current_city = "da";//$request->session()->get('current_city');
-
-        return view('news.index', compact('current_city'));
+        return view('news.index');
     }
 }
